32350155ce8fb8afa4896a7d391f49635f571c09
LonneQuent <lonnequentin@gmail.com>
2023-05-26 16:44:23 +0200

    maj streamlit

--- streamlit.py
+++ streamlit.py
@@ -5,35 +5,57 @@ import folium
 from streamlit_folium import folium_static
 
 # Exemple de dataframe avec des données en France
-data = pd.DataFrame({
-    'Ville': ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Bordeaux', 'Lille'],
-    'Latitude': [48.8567, 43.2964, 45.76, 43.6045, 43.7031, 44.8378, 50.6292],
-    'Longitude': [2.3522, 5.3699, 4.84, 1.444, 7.2661, -0.5792, 3.0573],
-    'Note': [3, 4, 2, 5, 3, 4, 5]
-})
+data = pd.read_csv('C:/Users/Aline/Documents/ProjetEntreprise/commentairesF.csv')
 
 # Titre de l'application
 st.title('Google Reviews en France')
 
 # Affichage du dataframe
-st.subheader('Données de notations')
+st.subheader('Données de notation')
 st.write(data)
 
 # Répartition des notes
 st.subheader('Répartition des notes')
 st.bar_chart(data['Note'].value_counts())
 
+# Supprimer les lignes avec des valeurs NaN dans les colonnes 'lat' et 'lng'
+data = data.dropna(subset=['lat', 'lng'])
+
+# Calculer la moyenne des notes par ville
+average_ratings = data.groupby('Ville')['Note'].mean().reset_index()
+
+# Trier les villes par ordre alphabétique
+sorted_cities = data['Ville'].sort_values().unique()
+
 # Création de la carte centrée sur la France
 m = folium.Map(location=[46.603354, 1.888334], zoom_start=6)
 
-# Ajout des marqueurs pour chaque ville
+# Ajout des marqueurs pour chaque ville avec la moyenne des notes dans le popup
 for index, row in data.iterrows():
+    ville = row['Ville']
+    lat = row['lat']
+    lng = row['lng']
+    note = average_ratings.loc[average_ratings['Ville'] == ville, 'Note'].values[0]
+    
     folium.Marker(
-        location=[row['Latitude'], row['Longitude']],
-        popup=row['Ville'] + ': ' + str(row['Note']),
+        location=[lat, lng],
+        popup=f"{ville}: {note:.2f}",
         icon=folium.Icon(color='blue')
     ).add_to(m)
 
 # Affichage de la carte
 st.subheader('Note moyenne par ville')
 folium_static(m)
+
+# Sélection de la ville
+selected_city = st.selectbox('Sélectionnez une ville', sorted_cities)
+
+# Filtrer les commentaires vides pour la ville sélectionnée
+selected_comments = data[data['Ville'] == selected_city]['Commentaire'].dropna()
+
+# Vérifier s'il y a des commentaires à afficher
+if not selected_comments.empty:
+    st.subheader(f"Commentaires pour {selected_city}")
+    st.write(selected_comments)
+else:
+    st.subheader(f"Aucun commentaire disponible pour {selected_city}")
