e8da62218221f34c55e9e1b1d32e23fdf7a5e1fd
LonneQuent <lonnequentin@gmail.com>
2023-05-30 15:26:19 +0200

    maj

--- streamlit.py
+++ streamlit.py
@@ -1,72 +1,70 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import folium
 from streamlit_folium import folium_static
-
-# Exemple de dataframe avec des données en France
-data = pd.read_csv('C:/Users/Aline/Documents/ProjetEntreprise/commentairesF.csv')
 
 # Titre de l'application
 st.title('Google Reviews en France')
 
-# Affichage du dataframe
-st.subheader('Données de notation')
-st.write(data)
+# Téléchargement du fichier
+uploaded_file = st.file_uploader("Télécharger le fichier CSV", type="csv")
 
-# Répartition des notes
-st.subheader('Répartition des notes')
-st.bar_chart(data['Note'].value_counts())
-
-# Supprimer les lignes avec des valeurs NaN dans les colonnes 'lat' et 'lng'
-data = data.dropna(subset=['lat', 'lng'])
-
-# Calculer la moyenne des notes par ville
-average_ratings = data.groupby('Ville')['Note'].mean().reset_index()
-
-# Trier les villes par ordre alphabétique
-sorted_cities = data['Ville'].sort_values().unique()
-
-# Création de la carte centrée sur la France
-m = folium.Map(location=[46.603354, 1.888334], zoom_start=6)
-
-# Ajout des marqueurs pour chaque ville avec la moyenne des notes dans le popup
-for index, row in data.iterrows():
-    ville = row['Ville']
-    lat = row['lat']
-    lng = row['lng']
-    note = average_ratings.loc[average_ratings['Ville'] == ville, 'Note'].values[0]
+# Vérification si un fichier a été téléchargé
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file, encoding='utf-8')
     
-    folium.Marker(
-        location=[lat, lng],
-        popup=f"{ville}: {note:.2f}",
-        icon=folium.Icon(color='blue')
-    ).add_to(m)
-
-# Affichage de la carte
-st.subheader('Note moyenne par ville')
-folium_static(m)
-
-# Créer une fonction pour filtrer les dates en fonction de la ville sélectionnée
-def get_available_dates(city):
-    dates = data[data['Ville'] == city]['Date'].unique()
-    return sorted(dates)
-
-# Sélection de la ville
-selected_city = st.selectbox('Sélectionnez une ville', sorted_cities)
-
-# Récupérer les dates disponibles pour la ville sélectionnée
-available_dates = get_available_dates(selected_city)
-
-# Sélection de la date
-selected_date = st.selectbox('Sélectionnez une date', available_dates)
-
-# Filtrer les commentaires pour la ville et la date sélectionnées
-filtered_comments = data[(data['Ville'] == selected_city) & (data['Date'] == selected_date)]['Commentaire'].dropna()
-
-# Vérifier s'il y a des commentaires à afficher
-if not filtered_comments.empty:
-    st.subheader(f"Commentaires pour {selected_city} le {selected_date}")
-    st.write(filtered_comments)
-else:
-    st.subheader(f"Aucun commentaire disponible pour {selected_city} le {selected_date}")
+    # Affichage du dataframe
+    st.subheader('Données de notation')
+    st.write(data)
+    
+    # Répartition des notes
+    st.subheader('Répartition des notes')
+    st.bar_chart(data['Note'].value_counts())
+    
+    # Supprimer les lignes avec des valeurs NaN dans les colonnes 'lat' et 'lng'
+    data = data.dropna(subset=['lat', 'lng'])
+    
+    # Calculer la moyenne des notes par ville
+    average_ratings = data.groupby('Ville')['Note'].mean().reset_index()
+    sorted_cities = data['Ville'].sort_values().unique()
+    
+    # Création de la carte centrée sur la France
+    m = folium.Map(location=[46.603354, 1.888334], zoom_start=6)
+    
+    # Ajout des marqueurs pour chaque ville avec la moyenne des notes dans le pop-up
+    for index, row in data.iterrows():
+        ville = row['Ville']
+        lat = row['lat']
+        lng = row['lng']
+        note = average_ratings.loc[average_ratings['Ville'] == ville, 'Note'].values[0]
+        
+        folium.Marker(
+            location=[lat, lng],
+            popup=f"{ville}: {note:.2f}",
+            icon=folium.Icon(color='blue')
+        ).add_to(m)
+    
+    # Affichage de la carte
+    st.subheader('Note moyenne par ville')
+    folium_static(m)
+    
+    selected_city = st.selectbox('Sélectionnez une ville', sorted_cities)
+    
+    # Groupement des données par ville et extraction des dates uniques pour chaque ville
+    dates_by_city = data.groupby('Ville')['Date'].unique()
+    
+    # Sélection de la date
+    selected_date = st.selectbox('Sélectionnez une date', dates_by_city[selected_city])
+    
+    # Filtrer les données en fonction de la ville et de la date sélectionnées
+    filtered_data = data[(data['Ville'] == selected_city) & (data['Date'] == selected_date)]
+    
+    # Filtrer les commentaires vides pour la ville sélectionnée
+    selected_comments = filtered_data['Commentaire'].dropna()
+    
+    # Vérifier s'il y a des commentaires à afficher
+    if not selected_comments.empty:
+        st.subheader(f"Commentaires pour {selected_city} le {selected_date}")
+        st.write(selected_comments)
+    else:
+        st.subheader(f"Aucun commentaire disponible pour {selected_city} le {selected_date}")
