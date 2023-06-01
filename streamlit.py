861989210f657c27bdb75c92ad05196163db2ac2
LonneQuent <lonnequentin@gmail.com>
2023-06-01 11:09:30 +0200

    maj streamlit

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ uploaded_file = st.file_uploader("Télécharger le fichier CSV", type="csv")
 
 # Vérification si un fichier a été téléchargé
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file, encoding='utf-8')
+    data = pd.read_csv(uploaded_file, encoding='utf-8', delimiter=';')
     
     # Affichage du dataframe
     st.subheader('Données de notation')
@@ -19,13 +19,13 @@ if uploaded_file is not None:
     
     # Répartition des notes
     st.subheader('Répartition des notes')
-    st.bar_chart(data['Note'].value_counts())
+    st.bar_chart(data['Note_x'].value_counts())
     
     # Supprimer les lignes avec des valeurs NaN dans les colonnes 'lat' et 'lng'
     data = data.dropna(subset=['lat', 'lng'])
     
     # Calculer la moyenne des notes par ville
-    average_ratings = data.groupby('Ville')['Note'].mean().reset_index()
+    average_ratings = data.groupby('Ville')['Note_x'].mean().reset_index()
     sorted_cities = data['Ville'].sort_values().unique()
     
     # Création de la carte centrée sur la France
@@ -36,7 +36,7 @@ if uploaded_file is not None:
         ville = row['Ville']
         lat = row['lat']
         lng = row['lng']
-        note = average_ratings.loc[average_ratings['Ville'] == ville, 'Note'].values[0]
+        note = average_ratings.loc[average_ratings['Ville'] == ville, 'Note_x'].values[0]
         
 
         if note <2 and note<3:
@@ -59,16 +59,16 @@ if uploaded_file is not None:
     selected_city = st.selectbox('Sélectionnez une ville', sorted_cities)
     
     # Groupement des données par ville et extraction des dates uniques pour chaque ville
-    dates_by_city = data.groupby('Ville')['Date'].unique()
+    dates_by_city = data.groupby('Ville')['Date_x'].unique()
     
     # Sélection de la date
     selected_date = st.selectbox('Sélectionnez une date', dates_by_city[selected_city])
     
     # Filtrer les données en fonction de la ville et de la date sélectionnées
-    filtered_data = data[(data['Ville'] == selected_city) & (data['Date'] == selected_date)]
+    filtered_data = data[(data['Ville'] == selected_city) & (data['Date_x'] == selected_date)]
     
     # Filtrer les commentaires vides pour la ville sélectionnée
-    selected_comments = filtered_data['Commentaire'].dropna()
+    selected_comments = filtered_data['Commentaire_x'].dropna()
     
     # Vérifier s'il y a des commentaires à afficher
     if not selected_comments.empty:
